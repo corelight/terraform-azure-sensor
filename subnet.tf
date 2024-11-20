af9b1e2660ff67a02db11e9e020bad9cffb569c3
Ryan Haney <25014745+thathaneydude@users.noreply.github.com>
2024-11-20 12:31:22 -0600

    Removing subnet creation from the module. We should not be altering the customers network. Updated the health check to use the appropriate 41080 port. renamed the sensor admin user to "ubuntu" to match our documentation. Added the Linux Health Extension to provide a grace period before health checks are sent.

--- subnet.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_subnet" "subnet" {
-  name                 = var.sensor_subnet_name
-  virtual_network_name = var.virtual_network_name
-  resource_group_name  = var.virtual_network_resource_group
-  address_prefixes = [
-    cidrsubnet(var.virtual_network_address_space, 8, 1)
-  ]
-}

2837bbdaa24403766a364d129cc07629bf7c464a
Ryan Haney <25014745+thathaneydude@users.noreply.github.com>
2025-01-23 09:34:55 -0600

    fmt

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   monitoring_subnet_resource_id_slice = split("/", var.monitoring_subnet_id)
-  monitoring_subnet_name      = local.monitoring_subnet_resource_id_slice[length(local.monitoring_subnet_resource_id_slice) - 1]
-  monitoring_subnet_vnet_name = local.monitoring_subnet_resource_id_slice[8]
+  monitoring_subnet_name              = local.monitoring_subnet_resource_id_slice[length(local.monitoring_subnet_resource_id_slice) - 1]
+  monitoring_subnet_vnet_name         = local.monitoring_subnet_resource_id_slice[8]
 
   monitoring_health_check_port = 41080
 

094bbaeff3474991ad799e0d64d1162e8f256116
Ryan Haney <25014745+thathaneydude@users.noreply.github.com>
2025-01-23 09:32:41 -0600

    VM extension port can't be a string and must be a number

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 locals {
   monitoring_subnet_resource_id_slice = split("/", var.monitoring_subnet_id)
-  monitoring_subnet_name              = local.monitoring_subnet_resource_id_slice[length(local.monitoring_subnet_resource_id_slice) - 1]
-  monitoring_subnet_vnet_name         = local.monitoring_subnet_resource_id_slice[8]
+  monitoring_subnet_name      = local.monitoring_subnet_resource_id_slice[length(local.monitoring_subnet_resource_id_slice) - 1]
+  monitoring_subnet_vnet_name = local.monitoring_subnet_resource_id_slice[8]
 
-  monitoring_health_check_port = "41080"
+  monitoring_health_check_port = 41080
 
   # https://learn.microsoft.com/en-us/azure/load-balancer/load-balancer-custom-probe-overview#probe-source-ip-address
   azure_lb_health_check_probe_ip = "168.63.129.16/32"

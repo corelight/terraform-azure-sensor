ebd42fea9422816cdbcf31f98e40b7bd3879576b
Ryan Haney <25014745+thathaneydude@users.noreply.github.com>
2025-02-06 12:30:39 -0600

    fixing data bug and updating README

--- sensor_config.tf
+++ sensor_config.tf
@@ -13,7 +13,7 @@ module "sensor_config" {
   sensor_monitoring_interface_name             = "eth1"
   sensor_health_check_probe_source_ranges_cidr = [local.azure_lb_health_check_probe_ip]
   sensor_health_check_http_port                = local.monitoring_health_check_port
-  subnetwork_monitoring_gateway                = cidrhost(data.azurerm_subnet.mon_subnet.address_prefix, 1)
+  subnetwork_monitoring_gateway                = cidrhost(data.azurerm_subnet.mon_subnet.address_prefixes[0], 1)
   subnetwork_monitoring_cidr                   = data.azurerm_subnet.mon_subnet.address_prefix
   gzip_config                                  = true
   base64_encode_config                         = true
